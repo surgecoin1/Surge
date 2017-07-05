141b26b995d963d8f784aa9e31ce2077373cc59a
surgecoin1 <surgecoin@gmail.com>
2017-07-05 12:46:18 +0200

    Update init.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -230,7 +230,7 @@ std::string HelpMessage()
 {
     string strUsage = _("Options:") + "\n" +
         "  -?                     " + _("This help message") + "\n" +
-        "  -conf=<file>           " + _("Specify configuration file (default: PX-Config.conf)") + "\n" +
+        "  -conf=<file>           " + _("Specify configuration file (default: Surgecoin.conf)") + "\n" +
         "  -pid=<file>            " + _("Specify pid file (default: Surge Coind.pid)") + "\n" +
         "  -datadir=<dir>         " + _("Specify data directory") + "\n" +
         "  -wallet=<dir>          " + _("Specify wallet file (within data directory)") + "\n" +
